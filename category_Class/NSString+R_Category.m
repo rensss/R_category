fbb1d58c1f85101ba57639aa471122bc0e07adf9
Rzk <renzzz@qq.com>
2019-07-31 17:04:53 +0800

    update tag 0.0.7

--- category_Class/NSString+R_Category.m
+++ category_Class/NSString+R_Category.m
@@ -277,4 +277,38 @@
     return YES;
 }
 
+
+/**
+ 时间转换  date转成xx years ago xx months ago
+ @param date 一个时间
+ @return 字符串
+ */
+- (NSString *)relativeDateStringForDate:(NSDate *)date
+{
+    NSCalendarUnit units = NSCalendarUnitDay | NSCalendarUnitWeekOfYear |
+    NSCalendarUnitMonth | NSCalendarUnitYear;
+    
+    // if `date` is before "now" (i.e. in the past) then the components will be positive
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:units
+                                                                   fromDate:date
+                                                                     toDate:[NSDate date]
+                                                                    options:0];
+    
+    if (components.year > 0) {
+        return [NSString stringWithFormat:@"%ld years ago", (long)components.year];
+    } else if (components.month > 0) {
+        return [NSString stringWithFormat:@"%ld months ago", (long)components.month];
+    } else if (components.weekOfYear > 0) {
+        return [NSString stringWithFormat:@"%ld weeks ago", (long)components.weekOfYear];
+    } else if (components.day > 0) {
+        if (components.day > 1) {
+            return [NSString stringWithFormat:@"%ld days ago", (long)components.day];
+        } else {
+            return @"Yesterday";
+        }
+    } else {
+        return @"Today";
+    }
+}
+
 @end
